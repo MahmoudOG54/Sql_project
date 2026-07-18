62b16b04dad1e12240d9f9c3d107b4462a779984
mahmoud elsayed <me4012956@gmail.com>
2026-07-18 16:54:33 +0300

    upload sql files

--- project/2_top_paying_jobs.sql
+++ project/2_top_paying_jobs.sql
@@ -1,10 +1,8 @@
+WITH top_paying_jobs AS(
 SELECT
     job_id,
     job_title,
-    job_location,
-    job_schedule_type,
     salary_year_avg,
-    job_posted_date,
     name AS company_name
 FROM
     job_postings_fact
@@ -14,4 +12,10 @@ job_title_short = 'Data Analyst' AND
 job_location = 'Anywhere' AND
 salary_year_avg IS NOT NULL
 ORDER BY salary_year_avg DESC
-LIMIT 10
+LIMIT 10)
+SELECT top_paying_jobs.*,
+skills
+FROM top_paying_jobs
+INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY salary_year_avg DESC
